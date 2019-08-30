e13a6dd1d513bf11767edfc7966f937880e7b451
Cristopher Dino <cristopher.dino@ahsay.com>
2019-08-30 13:42:30 +0800

    Disable zoom in zoom out

--- src/windows/fixwindowsize.js
+++ src/windows/fixwindowsize.js
@@ -6,9 +6,18 @@ cordova.commandProxy.add("windowResize",{
             var view = ApplicationView.getForCurrentView();
             view.tryResizeView({ width: 1024, height: 768 });
 
+            document.addEventListener("keydown", function(event){
+                if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+                    console.log(event);
+                    event.preventDefault();
+                 }
+            });
+            
+            document.addEventListener("mousewheel", function(event){console.log(event);event.preventDefault();}, { passive: false });
+
             window.addEventListener("resize", function(){
-                if(view.visibleBounds.width < 1024 || view.visibleBounds.height < 768){
-                    view.tryResizeView({ width: 1024, height: 768 });
+                if(view.visibleBounds.width < 959 || view.visibleBounds.height < 768){
+                    view.tryResizeView({ width: 959, height: 768 });
                 } else {
                     successCallback();
                 }
